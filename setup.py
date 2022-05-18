a55cafc6ac0a48b91066b129bfd92b323c61ea50
Ward Poelmans <wpoely86@gmail.com>
2022-05-18 17:27:56 +0200

    version bump
    Signed-off-by: Ward Poelmans <wpoely86@gmail.com>

--- setup.py
+++ setup.py
@@ -45,7 +45,7 @@ if sys.version_info < (3, 3):
     install_requires.append('ipaddress')
 
 PACKAGE = {
-    'version': '0.5.1',
+    'version': '0.5.2',
     'author': [ad],
     'maintainer': [ad],
     'setup_requires': ['vsc-install'],
