80c24c09eba502f67f80eaefcde0162c50ab29ee
Ward Poelmans <wpoely86@gmail.com>
2022-05-18 16:54:15 +0200

    add stuff for vsc-install

---
+++ test/__init__.py
@@ -0,0 +1,25 @@
+#
+# Copyright 2022-2022 Vrije Universiteit Brussel
+#
+# This file is part of vsc-filesystem-oceanstor,
+# originally created by the HPC team of Vrije Universiteit Brussel (https://hpc.vub.be),
+# with support of Vrije Universiteit Brussel (https://www.vub.be),
+# the Flemish Supercomputer Centre (VSC) (https://www.vscentrum.be),
+# the Flemish Research Foundation (FWO) (http://www.fwo.be/en)
+# and the Department of Economy, Science and Innovation (EWI) (http://www.ewi-vlaanderen.be/en).
+#
+# https://github.com/vub-hpc/vsc-filesystem-oceanstor
+#
+# vsc-filesystem-oceanstor is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation v2.
+#
+# vsc-filesystem-oceanstor is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with vsc-filesystem-oceanstor.  If not, see <http://www.gnu.org/licenses/>.
+#
+
