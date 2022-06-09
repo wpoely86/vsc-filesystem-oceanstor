5a18242540b18c8f6f20f0ad2a6d74da9a28feec
Alex Domingo <alex.domingo.toro@vub.be>
2022-06-09 11:38:40 +0200

    add backport of enum to install requires

--- setup.py
+++ setup.py
@@ -44,6 +44,9 @@ install_requires = [
 if sys.version_info < (3, 3):
     # Backport of the 3.3+ ipaddress module
     install_requires.append('ipaddress')
+if sys.version_info < (3, 4):
+    # Backport of the 3.4+ Enum module 
+    install_requires.append('enum34')
 
 PACKAGE = {
     'version': '0.6.0',
