c4709206d046ebbf0c58faccc11a895de722cfcc
Alex Domingo <alex.domingo.toro@vub.be>
2023-07-24 17:00:13 +0200

    check account access to object API once on init to avoid spamming unsuccessful queries to the interface

--- setup.py
+++ setup.py
@@ -49,7 +49,7 @@ if sys.version_info < (3, 4):
     install_requires.append('enum34')
 
 PACKAGE = {
-    'version': '0.8.1',
+    'version': '0.8.2',
     'author': [ad],
     'maintainer': [ad],
     'setup_requires': ['vsc-install'],
